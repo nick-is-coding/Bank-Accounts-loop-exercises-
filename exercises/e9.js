554dfb6a27487013b48848d448d12fa47619120f
Nick Hoang <nickjhoang@gmail.com>
2023-01-17 17:35:48 -0600

    finish e9

--- exercises/e9.js
+++ exercises/e9.js
@@ -5,8 +5,13 @@
 // getClientsWithBalanceOverOneHundred(array) => [{ name: 'Name1', balance: 32, ... }, { name: 'Name2', balance: 3523, ... }]
 
 export function getClientsWithBalanceOverOneHundred(array) {
-  // Your code goes here...
-
+  let overHundo = [];
+  for(let i = 0; i < array.length; i++){
+    if(array[i].balance > 100) {
+      overHundo.push(array[i]);
+    }
+  }
+  return overHundo;
 }
 
 
