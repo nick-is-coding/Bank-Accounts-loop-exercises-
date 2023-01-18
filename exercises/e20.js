183c603d4baa77ecd44f4e7a80b4b40e9ec2aa67
Nick Hoang <nickjhoang@gmail.com>
2023-01-18 14:52:23 -0600

    finish e20

--- exercises/e20.js
+++ exercises/e20.js
@@ -5,8 +5,24 @@
 // separateNamesWithAFromRest(["jon", "alice", "andrew", "mark", "jimmy"]) -> [["alice", "andrew", "mark"], ["jon", "jimmy"]]
 
 export function separateNamesWithAFromRest(array) {
-  // Your code goes here...
-
+  let namesWithA = [];
+  let namesWithoutA = [];
+  let finalArr = [namesWithA, namesWithoutA];
+  for(let i = 0; i < array.length; i++){
+    let hasA = false;
+    for(let j = 0; j < array[i].length; j++){
+      if(array[i][j] === 'a'){
+        hasA = true;
+        break;
+      }
+    }
+    if(hasA){
+      namesWithA.push(array[i]);
+    }else {
+      namesWithoutA.push(array[i]);
+    }
+  }
+  return finalArr;
 }
 
 
