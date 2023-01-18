cbc73b29ceacc698df6a52ac107cc3b3951954f4
Nick Hoang <nickjhoang@gmail.com>
2023-01-18 12:23:44 -0600

    finish e16

--- exercises/e16.js
+++ exercises/e16.js
@@ -6,8 +6,17 @@
 // NOTE: You can NOT use the array.flat() method in your code
 
 export function flatArrays(array) {
-  // Your code goes here...
-
+  let result = [];
+  for (let i = 0; i < array.length; i++) {
+      if (Array.isArray(array[i])) {
+          for (let j = 0; j < array[i].length; j++) {
+              result.push(array[i][j]);
+          }
+      } else {
+          result.push(array[i]);
+      }
+  }
+  return result;
 }
 
 
